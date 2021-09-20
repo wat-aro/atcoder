5f9477c66f29fb7167e982ebed176dcd44285901
wat-aro <kazutas1008@gmail.com>
2021-09-20 20:18:31 +0900

    Finish abc049c

---
+++ tasks/abc049c/src/main.rs
@@ -0,0 +1,37 @@
+// -*- coding:utf-8-unix -*-
+
+use proconio::input;
+
+fn main() {
+    input! {
+        s: String
+    }
+    if solve(s) {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
+}
+
+fn solve(s: String) -> bool {
+    let targets = ["dream", "dreamer", "erase", "eraser"];
+    let mut dp: [bool; 100000] = [false; 100000];
+    dp[0] = true;
+
+    for i in 0..s.len() {
+        if !dp[i] {
+            continue;
+        }
+
+        for &target in targets.iter() {
+            let length = target.len();
+            if i + length > s.len() {
+                continue;
+            } else if &s[i..i + target.len()] == target {
+                dp[i + target.len()] = true;
+            }
+        }
+    }
+
+    return dp[s.len()];
+}
---
+++ tasks/abc049c/tests/sample_inputs.rs
@@ -0,0 +1,48 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./abc049c";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"erasedream
+"#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "YES\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"dreameraser
+"#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "YES\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample3() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"dreamerer
+"#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "NO\n");
+    assert!(output.stderr_str().is_empty());
+}
