2aa9671344b27d8c4db96af353643612bff7e828
wat-aro <kazutas1008@gmail.com>
2021-09-26 07:27:23 +0900

    Finish abc086b

---
+++ tasks/abc086b/src/main.rs
@@ -0,0 +1,10 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        (a, b): (u8, u8)
+    }
+    let n = format!("{}{}", a, b).parse::<f64>().unwrap();
+    let m = n.sqrt();
+    println!("{}", if m == (m as u64) as f64 { "Yes" } else { "No" });
+}
---
+++ tasks/abc086b/tests/sample_inputs.rs
@@ -0,0 +1,48 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./abc086b";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"1 21
+"#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "Yes\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"100 100
+"#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "No\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample3() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"12 10
+"#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "No\n");
+    assert!(output.stderr_str().is_empty());
+}
