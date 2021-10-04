65038fb0e457a1f20cf8862f8027a061bd6b4c29
wat-aro <kazutas1008@gmail.com>
2021-10-04 23:51:46 +0900

    Finish abc138c

---
+++ tasks/abc138c/src/main.rs
@@ -0,0 +1,14 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        mut v: [i64; n]
+    }
+    v.sort();
+    let answer: f64 = v
+        .iter()
+        .fold(v[0] as f64, |acc, &x| (acc + x as f64) / 2 as f64);
+
+    println!("{}", answer);
+}
