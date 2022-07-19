f515ec1100aa037203503a72871e4213b58b5bc5
wat-aro <kazutas1008@gmail.com>
2022-07-19 23:30:55 +0900

    Fix abc108b

--- tasks/abc108b/src/main.rs
+++ tasks/abc108b/src/main.rs
@@ -4,7 +4,12 @@ fn main() {
     input! {
         (x1, y1, x2, y2): (i64, i64, i64, i64)
     }
-    let (x3, y3) = (x2 + y1 - y2, y2 + x2 - x1);
-    let (x4, y4) = (x1 + y1 - y2, y1 + x2 - x1);
+    let dx = x2 - x1;
+    let dy = y2 - y1;
+    let x3 = x2 - dy;
+    let y3 = y2 + dx;
+    let x4 = x1 - dy;
+    let y4 = y1 + dx;
+
     println!("{} {} {} {}", x3, y3, x4, y4);
 }
