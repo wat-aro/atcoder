901ad64990958ea1699a241fddbebac8f1830ee0
wat-aro <kazutas1008@gmail.com>
2022-07-25 15:29:19 +0900

    Update abc079b

--- tasks/abc079b/src/main.rs
+++ tasks/abc079b/src/main.rs
@@ -1,14 +1,30 @@
 use proconio::input;
 
+struct Lucus {
+    current: u64,
+    next: u64,
+}
+
+impl Iterator for Lucus {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let new_next = self.current + self.next;
+
+        self.current = self.next;
+        self.next = new_next;
+
+        Some(self.current)
+    }
+}
+
 fn main() {
     input! {
         n: usize,
     }
-    let mut v: Vec<u64> = Vec::new();
-    v.push(2);
-    v.push(1);
-    for i in 2..=n {
-        v.push(v[i - 1] + v[i - 2]);
-    }
-    println!("{}", v[n]);
+    let mut lucus = Lucus {
+        current: 2,
+        next: 1,
+    };
+    println!("{}", lucus.nth(n - 1).unwrap());
 }
