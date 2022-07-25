07f2a7728ba021bcc28bc557b1d3c8f38a55f443
wat-aro <kazutas1008@gmail.com>
2022-07-25 22:37:49 +0900

    Update abc139c

--- tasks/abc139c/src/main.rs
+++ tasks/abc139c/src/main.rs
@@ -5,11 +5,10 @@ fn main() {
         n: usize,
         mut h: [u64; n]
     }
-    h.reverse();
     let mut dp = vec![0; n];
-    for i in 1..n {
-        if h[i - 1] <= h[i] {
-            dp[i] = dp[i - 1] + 1
+    for i in (0..n - 1).rev() {
+        if h[i] >= h[i + 1] {
+            dp[i] = dp[i + 1] + 1
         }
     }
     println!("{}", dp.iter().max().unwrap());
