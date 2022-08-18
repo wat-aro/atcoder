ae29368e8a38bf0cf017ac634abddcc2ba08107b
wat-aro <kazutas1008@gmail.com>
2022-08-18 12:10:02 +0900

    Use simple loop in abc100c

--- tasks/abc100c/src/main.rs
+++ tasks/abc100c/src/main.rs
@@ -3,19 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a: [u64; n]
+        mut a: [u64; n]
     }
-    let answer: u64 = a.iter().map(|i| count2(*i)).sum();
+    let mut answer = 0;
+    for mut i in a {
+        while i % 2 == 0 {
+            answer += 1;
+            i /= 2;
+        }
+    }
 
     println!("{}", answer);
-}
-
-fn count2(n: u64) -> u64 {
-    let mut count = 0;
-    let mut m = n;
-    while m % 2 == 0 {
-        count += 1;
-        m = m / 2;
-    }
-    count
 }
