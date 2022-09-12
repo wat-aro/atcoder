2aa2e66ffbd25eae2abb62070ded31a18dcb2f85
wat-aro <kazutas1008@gmail.com>
2022-09-12 19:11:39 +0900

    Use char range

--- tasks/abc098b/src/main.rs
+++ tasks/abc098b/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use proconio::{input, marker::Chars};
 
 fn main() {
@@ -10,15 +8,11 @@ fn main() {
     let mut max: u64 = 0;
     for i in 1..(n - 1) {
         let mut count = 0;
-        let mut hs = HashSet::new();
-        s[0..i].iter().for_each(|c| {
-            hs.insert(c);
-        });
-        hs.iter().for_each(|c| {
-            if s[i..n].iter().any(|c1| *c == c1) {
+        for c in (b'a'..=b'z').map(char::from) {
+            if s[0..i].iter().any(|c1| c == *c1) && s[i..n].iter().any(|c1| c == *c1) {
                 count += 1;
             }
-        });
+        }
         max = max.max(count);
     }
 
