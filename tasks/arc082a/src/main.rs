9507c33e65583ba1e755d93e5a79b8191b698699
wat-aro <kazutas1008@gmail.com>
2022-09-15 21:18:44 +0900

    Update

--- tasks/arc082a/src/main.rs
+++ tasks/arc082a/src/main.rs
@@ -1,18 +1,22 @@
-use std::collections::HashMap;
-
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [i64; n]
+        a: [usize; n]
     }
-    let mut h: HashMap<i64, u64> = HashMap::new();
+    let mut v: Vec<usize> = vec![0; 100001];
     for ai in a {
-        *h.entry(ai - 1).or_insert(0) += 1;
-        *h.entry(ai).or_insert(0) += 1;
-        *h.entry(ai + 1).or_insert(0) += 1;
+        v[ai] += 1;
     }
-    let answer = h.values().max().unwrap();
-    println!("{}", answer);
+    let mut max = 0;
+    for i in 0..v.len() - 1 {
+        let sum = if i == 0 {
+            v[0] + v[1]
+        } else {
+            v[i - 1] + v[i] + v[i + 1]
+        };
+        max = max.max(sum);
+    }
+    println!("{}", max);
 }
