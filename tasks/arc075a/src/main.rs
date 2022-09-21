4fa90bb96d9f5207d3b71497bcb64bf2a64296db
wat-aro <kazutas1008@gmail.com>
2022-09-21 22:24:52 +0900

    Use dp

--- tasks/arc075a/src/main.rs
+++ tasks/arc075a/src/main.rs
@@ -3,17 +3,29 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: [u64; n]
+        s: [usize; n]
     }
-    let sum: u64 = s.iter().sum();
-
-    if sum % 10 == 0 {
-        if let Some(min) = s.iter().filter(|x| **x % 10 != 0).min() {
-            println!("{}", sum - min);
-        } else {
-            println!("0");
+    let num = 100 * 100;
+    let mut dp = vec![vec![0usize; num + 1]; n + 1];
+    for i in 0..n {
+        dp[i][0] = 1;
+    }
+    for i in 0..n {
+        for j in 1..=10000 {
+            if j >= s[i] && dp[i][j - s[i]] > 0 {
+                for k in (i + 1)..=n {
+                    dp[k][j] = dp[i][j - s[i]];
+                }
+            }
         }
-    } else {
-        println!("{}", sum);
     }
+
+    let answer = dp[n]
+        .iter()
+        .enumerate()
+        .filter(|(i, &x)| x > 0 && i % 10 != 0)
+        .max_by_key(|(i, _)| *i)
+        .unwrap_or((0, &0))
+        .0;
+    println!("{}", answer);
 }
