99aa19aeb07ef5a65bf784c102674efbf6e8c9cc
wat-aro <kazutas1008@gmail.com>
2022-09-23 22:07:22 +0900

    Update

--- tasks/abc088c/src/main.rs
+++ tasks/abc088c/src/main.rs
@@ -4,21 +4,30 @@ fn main() {
     input! {
         c: [[i16; 3]; 3]
     }
-    let b1 = c[0][0];
-    let b2 = c[0][1];
-    let b3 = c[0][2];
-    let a2 = c[1][0] - b1;
-    let a3 = c[2][0] - b1;
-
-    if a2 + b1 == c[1][0]
-        && a2 + b2 == c[1][1]
-        && a2 + b3 == c[1][2]
-        && a3 + b1 == c[2][0]
-        && a3 + b2 == c[2][1]
-        && a3 + b3 == c[2][2]
-    {
-        println!("Yes");
-    } else {
-        println!("No");
+    for a1 in 0..=*c[0].iter().min().unwrap() {
+        for a2 in 0..=*c[1].iter().min().unwrap() {
+            for a3 in 0..=*c[2].iter().min().unwrap() {
+                for b1 in 0..=((c[0][0] - a1).min(c[1][0] - a2).min(c[2][0] - a3)) {
+                    for b2 in 0..=(c[0][1] - a1).min(c[1][1] - a2).min(c[2][1] - a3) {
+                        for b3 in 0..=(c[0][2] - a1).min(c[1][2] - a2).min(c[2][2] - a3) {
+                            if a1 + b1 == c[0][0]
+                                && a1 + b2 == c[0][1]
+                                && a1 + b3 == c[0][2]
+                                && a2 + b1 == c[1][0]
+                                && a2 + b2 == c[1][1]
+                                && a2 + b3 == c[1][2]
+                                && a3 + b1 == c[2][0]
+                                && a3 + b2 == c[2][1]
+                                && a3 + b3 == c[2][2]
+                            {
+                                println!("Yes");
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
+    println!("No");
 }
